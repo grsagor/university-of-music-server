22b5beb77a3b289d2c4f4abf5d4dad1157ce3dfa
grsagor <grsagor08@gmail.com>
2022-11-11 04:22:38 +0600

    data can loaded from mongodb

--- index.js
+++ index.js
@@ -13,11 +13,24 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7le8ogp.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+const run = async() => {
+    try{
+        const serviceCollection = client.db('assignment11').collection('services');
+        
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+        
+    }
+};
+
+run().catch(err => console.log(err));
 
 
 console.log(process.env.DB_USER);
